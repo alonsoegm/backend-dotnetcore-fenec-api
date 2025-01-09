3ac9adcd3f36cbdb63320fbe44a066e34265e916
alonsoegm <agallegos@hitachisolutions.com>
2025-01-09 17:53:04 -0600

    Create healthController

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace FenecApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HealthController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+
+		public HealthController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		[HttpGet("db-connection")]
+		public IActionResult CheckDatabaseConnection()
+		{
+			try
+			{
+				var connectionString = _configuration.GetConnectionString("DefaultConnection");
+				using var connection = new SqlConnection(connectionString);
+				connection.Open();
+
+				return Ok("Connection to the database is successful.");
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"Database connection failed: {ex.Message}");
+			}
+		}
+	}
+}
